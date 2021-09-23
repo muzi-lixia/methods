ef41af2d96afa8f935d98c3c413af09cf60c73a9
muzi <muzi_lixia@163.com>
2021-09-23 11:46:03 +0800

    update the instanceof method

--- 封装/new、instanceof.js
+++ 封装/new、instanceof.js
@@ -6,6 +6,13 @@
         2.新对象需要能够访问到构造函数的属性，所以需要重新指定它的原型
         3.构造函数可能显示返回
 */
+/* 
+    MDN对于new关键字的描述
+        1.创建一个空的简单JavaScript对象（即{}）
+        2.为步骤1新创建的对象添加属性__proto__，将该属性连接至构造函数的原型对象
+        3.将步骤1新创建的对象作为this的上下文
+        4.如果该函数没有返回对象，则返回this上下文
+*/
 function objectFactory() {
     let obj = new Object();
     let Constructor = [].shift.call(arguments);
@@ -25,6 +32,8 @@ console.log(objectFactory(person, '哈哈', 18)); // ==> person {name: "哈哈"
     instanceof就是判断构造函数的prototype属性是否在实例的原型链上
 */
 function my_instanceof(left, right) {
+    const baseType = ['string', 'number','boolean','undefined','symbol']
+    if(baseType.includes(typeof left)) return false;
     // let proto = left.__proto__;
     let proto = Object.getPrototypeOf(left);
     while(true) {
